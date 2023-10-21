70dd150c51bf0f47aa5a4261d60f64173dc67821
LordChiki18 <cristiandomecqc@gmail.com>
2023-10-21 18:42:17 -0300

    For hugo

--- aplicaciones/cuentas/urls.py
+++ aplicaciones/cuentas/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from aplicaciones.cuentas.views import CiudadViews, PersonaViews, ClienteViews, CuentasViews
+
+from aplicaciones.cuentas import views
+from aplicaciones.cuentas.views import CiudadViews, PersonaViews, ClienteViews, CuentasViews, index
 
 router = DefaultRouter()
 
@@ -10,5 +12,6 @@ router.register(r'Cliente', ClienteViews)
 router.register(r'Cuentas', CuentasViews)
 
 urlpatterns = [
+    path('', views.index),
     path('v1/', include(router.urls)),
 ]
