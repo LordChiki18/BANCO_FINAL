1f5b086016cd73780959140b1a3ecd9097c99af6
LordChiki18 <cristiandomecqc@gmail.com>
2023-10-24 20:50:10 -0300

    Vistas de transferencias URLS Correspondientes a transferencias Se agrego el IsAuthenticated a las vistas

--- aplicaciones/cuentas/urls.py
+++ aplicaciones/cuentas/urls.py
@@ -2,7 +2,8 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
 from aplicaciones.cuentas import views
-from aplicaciones.cuentas.views import CiudadViews, PersonaViews, ClienteViews, CuentasViews, index,iniciar_sesion,cuentas_page
+from aplicaciones.cuentas.views import CiudadViews, PersonaViews, ClienteViews, CuentasViews, index, iniciar_sesion, \
+    cuentas_page, TransferenciasView
 
 router = DefaultRouter()
 
@@ -13,7 +14,8 @@ router.register(r'Cuentas', CuentasViews)
 
 urlpatterns = [
     path('', views.index),
-    path('accounts/login/',views.iniciar_sesion, name='iniciar_sesion'),
+    path('accounts/login/', views.iniciar_sesion, name='iniciar_sesion'),
     path('cuentas/', views.cuentas_page, name='cuentas_page'),
-    path('v1/', include(router.urls)),
+    path('gestiones/', include(router.urls)),
+    path('finanzas/transferencias', TransferenciasView.as_view()),
 ]
