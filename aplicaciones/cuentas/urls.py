b5e99b619bfcce12f1348081d3c787801669f960
hugoprograma324 <140003861+hugoprograma324@users.noreply.github.com>
2023-10-23 22:35:37 -0300

    Revert "Actualizaciones"

---
+++ aplicaciones/cuentas/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from aplicaciones.cuentas import views
+from aplicaciones.cuentas.views import CiudadViews, PersonaViews, ClienteViews, CuentasViews, index,iniciar_sesion,cuentas_page
+
+router = DefaultRouter()
+
+router.register(r'Ciudad', CiudadViews)
+router.register(r'Persona', PersonaViews)
+router.register(r'Cliente', ClienteViews)
+router.register(r'Cuentas', CuentasViews)
+
+urlpatterns = [
+    path('', views.index),
+    path('accounts/login/',views.iniciar_sesion, name='iniciar_sesion'),
+    path('cuentas/', views.cuentas_page, name='cuentas_page'),
+    path('v1/', include(router.urls)),
+]
