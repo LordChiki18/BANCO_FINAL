667913494b0e14d1c14cc13290cde339e9365d50
hugoprograma324 <140003861+hugoprograma324@users.noreply.github.com>
2023-10-25 22:36:35 -0300

    git commit

--- aplicaciones/cuentas/forms.py
+++ aplicaciones/cuentas/forms.py
@@ -1,15 +1,17 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
+from .models import Ciudad,Persona
 
 class RegistroForm(UserCreationForm):
-    nombre = forms.CharField(max_length=255)
-    apellido = forms.CharField(max_length=255)
+    ciudad_id = forms.ModelChoiceField(queryset=Ciudad.objects.all(), widget=forms.Select(attrs={'placeholder': 'Ciudad'}))
+    nombre = forms.CharField(max_length=255, widget=forms.TextInput(attrs={'placeholder': 'Nombre'}))
+    apellido = forms.CharField(max_length=255, widget=forms.TextInput(attrs={'placeholder': 'Apellido'}))
     tipo_documento = forms.ChoiceField(choices=(
         ('Pasaporte', 'Pasaporte'),
         ('RUC', 'RUC'),
         ('CI', 'CI'),
-    ))
+    ), widget=forms.Select(attrs={'placeholder': 'Tipo de Documento'}))
     numero_documento = forms.CharField(max_length=255, widget=forms.TextInput(attrs={'placeholder': 'Ejemplo: 12345678'}))
-    direccion = forms.CharField(max_length=255)
+    direccion = forms.CharField(max_length=255, widget=forms.TextInput(attrs={'placeholder': 'Dirección'}))
     celular = forms.CharField(max_length=255, widget=forms.TextInput(attrs={'placeholder': 'Ejemplo: 1234567890'}))
-    email = forms.EmailField()
+    email = forms.EmailField(widget=forms.TextInput(attrs={'placeholder': 'Email'}))
